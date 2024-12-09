d0911d4c5fb1ea3609ab1f4b1afba636a1f31e4d
Eloy Rojano Vargas <rojanovargas@Mac-mini-de-Eloy.local>
2024-12-09 16:02:25 +0100

    press and filling working

--- version01/App.js
+++ version01/App.js
@@ -10,44 +10,87 @@ export default function App() {
 	const onPressA = () => setCountA((prevCount) => prevCount + 1)
 	const onPressB = () => setCountB((prevCount) => prevCount + 1)
 
+	const total = countA + countB
+
+	let flexOptionA = countA
+	let flexOptionB = countB
+
+	if (countA === 0 && countB === 0) {
+		flexOptionA = 1
+		flexOptionB = 1
+	}
+
+	const styles = createStyles(flexOptionA, flexOptionB)
+
 	return (
 		<View style={styles.mainContainer}>
-			<TouchableOpacity style={styles.containerOptionA} onPress={onPressA}>
-				<Text>Option A</Text>
-				<Text>{countA}</Text>
+			<TouchableOpacity
+				style={styles.containerOptionA}
+				onLongPress={onPressA}
+				activeOpacity={0.8}
+			>
+				<Text style={styles.h2}>Option A</Text>
+				<Text style={styles.paragraph}>
+					{((countA / total) * 100).toFixed(2) === "NaN"
+						? "Hold to vote"
+						: `${((countA / total) * 100).toFixed(2)}%`}
+				</Text>
 			</TouchableOpacity>
-			<TouchableOpacity style={styles.containerOptionB} onPress={onPressB}>
-				<Text>Option B</Text>
-				<Text>{countB}</Text>
+			<View style={styles.divider} />
+			<TouchableOpacity
+				style={styles.containerOptionB}
+				onLongPress={onPressB}
+				activeOpacity={0.8}
+			>
+				<Text style={styles.h2}>Option B</Text>
+				<Text style={styles.paragraph}>
+					{((countB / total) * 100).toFixed(2) === "NaN"
+						? "Hold to vote"
+						: `${((countB / total) * 100).toFixed(2)}%`}
+				</Text>
 			</TouchableOpacity>
 			<StatusBar style="auto" />
 		</View>
 	)
 }
 
-const totalFlex = 10
-const flexOptionA = 1.3
-const flexOptionB = totalFlex - flexOptionA
-
-const styles = StyleSheet.create({
-	mainContainer: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	containerOptionA: {
-		flex: flexOptionA,
-		backgroundColor: "lightgreen",
-		alignItems: "center",
-		justifyContent: "center",
-		width: "100%",
-	},
-	containerOptionB: {
-		flex: flexOptionB,
-		backgroundColor: "orange",
-		alignItems: "center",
-		justifyContent: "center",
-		width: "100%",
-	},
-})
+const createStyles = (flexOptionA, flexOptionB) =>
+	StyleSheet.create({
+		mainContainer: {
+			flex: 1,
+			backgroundColor: "#fff",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+		containerOptionA: {
+			flex: flexOptionA,
+			backgroundColor: "lightgreen",
+			alignItems: "center",
+			justifyContent: "center",
+			width: "100%",
+		},
+		containerOptionB: {
+			flex: flexOptionB,
+			backgroundColor: "orange",
+			alignItems: "center",
+			justifyContent: "center",
+			width: "100%",
+		},
+		h2: {
+			fontSize: 32,
+			textShadowColor: "rgba(0, 0, 0, 0.75)",
+			textShadowOffset: { width: -1, height: 1 },
+			textShadowRadius: 10,
+			color: "white",
+			fontWeight: 700,
+		},
+		paragraph: {
+			fontSize: 20,
+			fontWeight: 700,
+		},
+		divider: {
+			height: 4,
+			width: "100%",
+			backgroundColor: "#000",
+		},
+	})
